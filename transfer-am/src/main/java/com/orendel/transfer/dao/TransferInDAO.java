f5d0dbaf52e4f4bf0c4d0dafad40434a1b9d7220
Carlos <carlosal_p@yahoo.com>
2015-04-16 02:05:57 -0500

    Se modifica el query de actualización de Counterpoint para actualizar el campo QTY_ON_XFER_IN de la tabla IM_INV solamente cuando el valor existente sea mayor a 0. Se agrega un session.flush() para garantizar la actualización de tablas de transfer in e inventario.

--- transfer-am/src/main/java/com/orendel/transfer/dao/TransferInDAO.java
+++ transfer-am/src/main/java/com/orendel/transfer/dao/TransferInDAO.java
@@ -12,6 +12,7 @@ public class TransferInDAO extends GenericDAOImpl<TransferIn, Long> {
 		getSession().beginTransaction();
 		updateCounterPointTxInLine(transferIn);
 		getSession().saveOrUpdate(transferIn);
+		getSession().flush();
 		getSession().getTransaction().commit();
 	}
 	
@@ -19,7 +20,11 @@ public class TransferInDAO extends GenericDAOImpl<TransferIn, Long> {
 	private void updateCounterPointTxInLine(TransferIn transferIn) {
 		for (TransferInLine line : transferIn.getLines()) {
 			String updateTable_IM_INV = "update IM_INV set qty_commit = qty_commit - :qtyReceived, "
-					+ "qty_on_xfer_in = qty_on_xfer_in - :qtyReceived "
+					+ "qty_on_xfer_in = "
+					+ "(case "
+					+ "   when qty_on_xfer_in > 0 then qty_on_xfer_in - :qtyReceived "
+					+ "   else qty_on_xfer_in "
+					+ "end) "
 					+ "where item_no = :itemNo and loc_id = :toLocation";
 			Query query = getSession().createSQLQuery(updateTable_IM_INV);
 			query.setParameter("qtyReceived", line.getQtyReceived());
