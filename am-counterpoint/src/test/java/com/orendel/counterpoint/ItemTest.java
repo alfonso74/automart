cf7a600e2d7315b922855f96cfe154770a419ea7
Carlos <carlosal_p@yahoo.com>
2015-10-02 23:48:44 -0500

    Prueba unitaria para el objeto Item.

---
+++ am-counterpoint/src/test/java/com/orendel/counterpoint/ItemTest.java
@@ -0,0 +1,26 @@
+package com.orendel.counterpoint;
+
+import static org.junit.Assert.*;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import org.junit.Test;
+
+import com.orendel.counterpoint.domain.Item;
+
+public class ItemTest {
+	
+	
+	@Test
+	public void testGetUpdatedDateLabel_Happy() {
+		Calendar calendar = new GregorianCalendar();
+		calendar.clear();
+		calendar.set(2014, Calendar.MAY, 22);
+		Item item = new Item();
+		item.setUpdated(calendar.getTime());
+		String labelForPrinter = item.getUpdatedDateLabel();
+		assertTrue(labelForPrinter.equalsIgnoreCase("A14H05T22"));		
+	}
+
+}
